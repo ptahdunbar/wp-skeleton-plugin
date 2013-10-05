510feb53939b66b03d9282999f509de983f65a27
Ptah Dunbar <ptah@piratedunbar.com>
2013-10-05 07:41:06 +0200

    Initial import

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-wp-skeleton-plugin
-==================
+# WP Skeleton Plugin project - WIP.
 
-A WordPress Skeleton Plugin
+This is simply a skeleton repo for a WordPress plugin. Use it to jump-start your WordPress plugin, or fork it and customize it to your own liking!
+
+## Installation
+
+```
+git clone git@github.com:ptahdunbar/wp-skeleton-plugin.git
+```
---
+++ tests/WPSkeletonPluginTest/AutoloadTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace WPSkeletonPluginTest;
+
+class AutoloadTest extends \PHPUnit_Framework_TestCase
+{
+	public function testThatItsTestingTime()
+    {
+//        $this->fail('What time is it? - it\'s testing time! :D');
+    }
+
+    public function testing()
+    {
+        $this->assertTrue(true);
+
+        $this->assertSame();
+        $this->assertGreaterThan();
+
+        $u = $this
+            ->getMock('sds');
+
+        $u->expects($this->)
+    }
+}
