35aa8645600fa6c9dc3f4c9eb740107e6df573bc
Ptah Dunbar <ptah@piratedunbar.com>
2013-10-05 08:01:09 +0200

    Remove test cruff

--- tests/WPSkeletonPluginTest/AutoloadTest.php
+++ tests/WPSkeletonPluginTest/AutoloadTest.php
@@ -6,19 +6,6 @@ class AutoloadTest extends \PHPUnit_Framework_TestCase
 {
 	public function testThatItsTestingTime()
     {
-//        $this->fail('What time is it? - it\'s testing time! :D');
-    }
-
-    public function testing()
-    {
-        $this->assertTrue(true);
-
-        $this->assertSame();
-        $this->assertGreaterThan();
-
-        $u = $this
-            ->getMock('sds');
-
-        $u->expects($this->)
+        $this->fail('What time is it? - it\'s testing time! :D');
     }
 }
