50a51b2498e76761aace47ec00727a1b183fbf74
Pirate Dunbar <yarr@piratedunbar.com>
2013-11-07 17:15:52 -0500

    Update AutoloadTest.php

--- tests/WPSkeletonPluginTest/AutoloadTest.php
+++ tests/WPSkeletonPluginTest/AutoloadTest.php
@@ -4,8 +4,8 @@ namespace WPSkeletonPluginTest;
 
 class AutoloadTest extends \PHPUnit_Framework_TestCase
 {
-	public function testThatItsTestingTime()
+    public function testThatItsTestingTime()
     {
-        $this->fail('What time is it? - it\'s testing time! :D');
+        $this->assertTrue(true);
     }
 }
