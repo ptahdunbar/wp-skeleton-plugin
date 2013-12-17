5725d958792fe0e4fe03372437e13d07f9671ee1
Ptah Dunbar <yarr@piratedunbar.com>
2013-12-17 00:08:40 -0500

    Closer... much closer

--- autoload.php
+++ autoload.php
@@ -1,13 +1,14 @@
 <?php
 /**
- * Plugin Name : WP Skeleton Plugin
- * Version : 1.0.0
- * Description : A WordPress skeleton plugin. -_O
- * Author : Ptah Dunbar
- * Author URI : http://ptahdunbar.com
- * Text Domain : wp-skeleton-plugin
- * Domain Path : /languages
- * License : GPL2 or later
+ * Plugin Name: WP Skeleton Plugin
+ * Version: 1.0.0
+ * Description: A WordPress skeleton plugin. -_O
+ * Author: Ptah Dunbar
+ * Author URI: http://ptahdunbar.com
+ * Text Domain: wp-skeleton-plugin
+ * Domain Path: /languages
+ * License: GPL2 or later
+ * GitHub Plugin URI: https://github.com/<owner>/<repo>
  *
  * @package WPSkeletonPlugin
  */
