2b875fe0cb1d0a677d32faa9e930652e59c25776
Pirate Dunbar <yarr@piratedunbar.com>
2013-12-30 10:57:29 -0500

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,15 +1,17 @@
 <?php
-/**
+ /**
  * Plugin Name: WP Skeleton Plugin
+ * Plugin URI: https://github.com/ptahdunbar/wp-skeleton-plugin
+ * Description: A walking skeleton WordPress plugin. -_O
  * Version: 0.0.1
- * Description: A WordPress skeleton plugin. -_O
  * Author: Ptah Dunbar
  * Author URI: http://ptahdunbar.com
  * Text Domain: wp-skeleton-plugin
- * Domain Path: /languages
- * License: GPL2 or later
+ * Domain Path: languages/
  * GitHub Plugin URI: <owner>/<repo>
  * GitHub Branch: master
+ * License: GPL2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 /** Load composer */
