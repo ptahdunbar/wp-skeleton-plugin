99fdb415839d8a9dacd0cba6cf0015151e9c0405
Pirate Dunbar <yarr@piratedunbar.com>
2013-12-30 12:14:14 -0500

    Update strings.php

--- languages/strings.php
+++ languages/strings.php
@@ -1,10 +1,6 @@
 <?php
 /**
  * Use this file to add strings meant for localisation.
- *
- * @category    i18n
- * @package     WPSkeletonPlugin/i18n
- * @version     1.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
